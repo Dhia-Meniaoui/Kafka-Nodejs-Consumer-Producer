e2eb1cefc4e269574e2f9fe19149c8187f802c56
dhia <diaminiaoui@gmail.com>
2022-04-21 03:04:49 +0100

    21/04

--- consumer/index.js
+++ consumer/index.js
@@ -1,1 +1,29 @@
-console.log("consumer");
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092']
+})
+
+
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const runConsumer = async () => {
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic-2', fromBeginning: true })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                partition,
+                offset: message.offset,
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+
+runConsumer()
+    .catch(err => {
+        console.log(err)
+    })
--- producer/index.js
+++ producer/index.js
@@ -1,24 +1,43 @@
-import { Kafka } from "kafkajs";
-
+import { Kafka } from 'kafkajs';
 
 const kafka = new Kafka({
     clientId: 'my-app',
-    brokerss:[ 'localhost:9092' , 'localhost:9093' , 'localhost:9094']
+    brokers: ['localhost:9092']
 })
 
 
 const producer = kafka.producer()
 producer.connect()
     .then(res => {
-        console.log("porducer server successfully started ");
+        console.log('producer server successfully started')
     })
     .catch(err => {
-        console.log('prodcure server failed ');
+        console.log('producer server failed')
     })
 
 
-const queueMessage = async ( order ) => {
+const queueMessage = async (order) => {
     return await producer.send({
-        topic: "test-topic-2",
+        topic: 'test-topic-2',
+        messages: [
+            { value: JSON.stringify(order) },
+        ],
     })
+
 }
+
+setInterval(() => {
+    const newOrder = {
+        message: "messsage",
+        Number: "1",
+        id: 1
+    }
+    queueMessage(newOrder)
+        .then(() => {
+            console.log('sended message:', newOrder)
+
+        })
+        .catch(err => {
+            console.log('failed message:', newOrder)
+        })
+}, 5000)
